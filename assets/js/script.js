ea2b6cc7d2eba0a98a2a85dcd7353d00eb9f5686
flokamp <74511633+flokamp@users.noreply.github.com>
2020-12-21 10:03:17 -0600

    combined password prompts into passwordPrompts function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,25 @@
-var choiceArray = []; 
-var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var number = ['1','2','3','4','5','6','7','8','9','0'];
-var symbol = ['!','@','#','$','%','^','&','*'];
-  
+var promptLength = [];
+var choiceArr = []; 
+var upperArr= ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var lowerArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var numberArr = ['1','2','3','4','5','6','7','8','9','0'];
+var symbolArr = ['!','@','#','$','%','^','&','*'];
 
 // function to get password length from user
-var passwordLength = function() {
+var passwordPrompts = function() {
   var promptLength = parseInt(prompt('How many characters would you like your password to be? Enter a number between 1 and 128.'));
     // invalid input
-    if (promptLength === "" || promptLength === null || promptLength < 8 || promptLength > 128) {
+    while ( isNaN(promptLength) || promptLength === "" || promptLength === null || promptLength < 8 || promptLength > 128) {
       window.alert("Please enter a number between 1 and 128.")
-      return passwordLength();
-    } else {
+      passwordPrompts();
+    } 
       window.alert("Your password will be " + promptLength + " characters long.")
-      // check password length
-      console.log(promptLength)
-      passwordCharacters();
-    }
-  return promptLength;
-}
-
-
-// function to get character types from user
-var passwordCharacters = function() {
-
+  
   // ask if user wants to include uppercase characters
   var confirmUpper = confirm("Would you like to use uppercase letters?");
     if (confirmUpper) {
       // add uppercase letters to array
-      choiceArray.push(randomUpper);
+      choiceArr = choiceArr.concat(upperArr);
       window.alert("Your password will include uppercase letters.")
     } else {
       window.alert("Your password will not include uppercase letters.")
@@ -41,7 +31,7 @@ var passwordCharacters = function() {
   var confirmLower = confirm("Would you like to use lowercase letters?");
     if (confirmLower) {
       // add lowercase letters to array
-      choiceArray.push(randomLower);
+      choiceArr = choiceArr.concat(lowerArr);
       window.alert("Your password will include lowercase letters.")
     }
     else {
@@ -54,7 +44,7 @@ var passwordCharacters = function() {
   var confirmNumber = confirm("Would you like to use numbers?");
     if (confirmNumber) {
       // add numbers to array
-      choiceArray.push(randomNumber);
+      choiceArr = choiceArr.concat(numberArr);
       window.alert("Your password will include numbers.")
     }
     else {
@@ -65,13 +55,14 @@ var passwordCharacters = function() {
 
   // automatically set password to use symbols if all other character types are false
     if (confirmUpper === false && confirmLower === false && confirmNumber === false) {
+        choiceArr = choiceArr.concat(symbolArr);
         window.alert("Your password will only include symbols.")
     }
     else {
       var confirmSymbol = confirm("Would you like to use symbols?")
       if (confirmSymbol) {
         // add symbols to array
-        choiceArray.push(randomSymbol);
+        choiceArr = choiceArr.concat(symbolArr);
         window.alert("Your password will include symbols.")
       }
       else {
@@ -80,36 +71,35 @@ var passwordCharacters = function() {
       // check to see if symbol is true or false
       console.log(confirmSymbol)
     }
+    return choiceArr;
+}
+
+
+
+// function to generate password using chosen characters
+function generatePassword() {
+  var password= "";
+  for(var i = 0; i < promptLength; i++) {
+    var randomCharacter = Math.floor(Math.random() * choiceArr.length);
+    password = password + choiceArr[randomCharacter];
   }
- 
-  // functions to generate random characters
-  var randomUpper = function() {
-   return uppercase[Math.floor(Math.random() * uppercase.length)];
-  }
-  
-  // function to generate random lowercase letter
-  var randomLower = function(){
-    return lowercase[Math.floor(Math.random() * lowercase.length)];
-  }
-  
-  // function to generate random number
-  var randomNumber = function(){
-    return number[Math.floor(Math.random() * number.length)];
-  }
-  
-  // function to generate random symbol
-  var randomSymbol = function(){
-    return symbol[Math.floor(Math.random() * symbol.length)];
-  }
-  
+  return password;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var promptsExist = passwordPrompts();
 
-  passwordText.value = password;
+  if (promptsExist) {
+   var newPassword = generatePassword();
+   var passwordText = document.querySelector("#password");
+
+  passwordText.value = newPassword;
+  }
 }
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
